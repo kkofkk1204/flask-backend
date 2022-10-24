95cea87c04d33390bec96bde78b99071dbe3413c
kkofkk1204 <101874345+kkofkk1204@users.noreply.github.com>
2022-10-24 17:04:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -17,14 +17,15 @@ def home():
 def testing_function():
     DATABASE_URL  = 'mysql://admin:12345678@flask-backend.ckxv07dhtiyg.ap-northeast-1.rds.amazonaws.com:3306/'
     engine = create_engine(DATABASE_URL)
-    with engine.connect() as connection:
-        connection.execute("use flaskbackend")
-        query = text("SELECT * FROM ecdsa")
-        blog_posts = connection.execute(query)
-        x = []
-        for post in blog_posts:
-            x.append(post)
-        print(x)
+    # with engine.connect() as connection:
+    #     connection.execute("use flaskbackend")
+    #     query = text("SELECT * FROM ecdsa")
+    #     blog_posts = connection.execute(query)
+    #     x = []
+    #     for post in blog_posts:
+    #         x.append(post)
+    #     print(x)
+    x = "123"
     return jsonify({"message" : str(x)}),200
 
 
